692ff653e15d55c7ecbee9f03ce377a96e9accc3
unknown <20230450+HDR@users.noreply.github.com>
2020-06-18 22:15:55 +0200

    Update JoeyFirmwareTool.cpp

--- Firmware/JoeyFirmwareTool.cpp
+++ Firmware/JoeyFirmwareTool.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+#include <iomanip>
 
 int main (int argc, char* argv[]) {
-	std::ifstream ifs(argv[1]);  //One call to start it all. . .
-	ifs.seekg(-32768, std::ios_base::end);  // One call to find it. . .
-	char tmpBuffer[32768];
-	ifs.read(tmpBuffer, 32768);  //One call to read it all. . .
+	std::ifstream ifs(argv[1]);
+	ifs.seekg(512, std::ios_base::beg);
+	char tmpBuffer[33280];
+	ifs.read(tmpBuffer, 32768);
 	ifs.close();
-	std::ofstream ofs("JoeyFirmware.bin", std::ios::trunc);
-	ofs.write(tmpBuffer, 32768); //And to the FS bind it.
-	
+	std::ofstream ofs("JoeyFirmware.bin", std::ios_base::binary | std::ios::trunc);
+	ofs.write(tmpBuffer, 32768);
 	return 0;
 }
